4ef66445c5860ad6afefc7bc00e6de8ecf27efdb
hama <a-hama@zj.nes.jp.nec.com>
2017-06-28 15:13:52 +0900

    0628

---
+++ src/com/nec/webotx/webapp/tutorial/HelloBean.java
@@ -0,0 +1,25 @@
+package com.nec.webotx.webapp.tutorial;
+
+public class HelloBean {
+
+	private String name;
+
+	private int score;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+}
